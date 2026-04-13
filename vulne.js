8be8da7c9364b01fd608d34d19334fc9d7aa6de9
ajhernandezr <36048704+ajhernandezr@users.noreply.github.com>
2026-04-13 11:17:30 +0200

    Delete vulne.js

--- vulne.js
+++
@@ -1,26 +0,0 @@
-const express = require('express');
-const { execFile } = require('child_process');
-const app = express();
-
-app.get('/user', (req, res) => {
-    const input = req.query.name;
-
-    // ❌ XSS
-    res.send("<h1>Hello " + input + "</h1>");
-
-    // ❌ Command Injection
-    execFile('ls', [input], (error) => {
-        if (error) {
-            console.error(error);
-        }
-    });
-
-    // ❌ Path Traversal
-    const fs = require('fs');
-    const data = fs.readFileSync("/var/data/" + input, "utf8");
-
-    res.send(data);
-});
-
-app.listen(3000);
-
